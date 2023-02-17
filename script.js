4abfa17e554f0b3a0147626ee8b03f3a94d72ae5
SreeHarshan <sreeerode12@gmail.com>
2023-02-17 11:14:04 +0530

    changed little stuff

--- script.js
+++ script.js
@@ -4,8 +4,4 @@ window.addEventListener("load",function(){
 function close_ad(){
     document.getElementById("popup").style.display="none";
 }
-const values = document.querySelectorAll('[class=class1]');
-values.forEach(function(v) {
-  console.log(v.innerHTML);
-}); 
-//document.getElementById("close").addEventListener("click",close);
+
